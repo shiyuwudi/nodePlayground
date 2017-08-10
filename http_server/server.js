9caeda2ebca11b5e62db0e9b6375473ea3c2e2ae
石宇 <shiyu@shiyudeMacBook-Pro.local>
2017-08-10 09:06:58 +0800

    first commit

---
+++ http_server/server.js
@@ -0,0 +1,7 @@
+var http = require("http");
+http.createServer(function(req, resp){
+    resp.writeHead(200, {"Content-Type": "text/plain"});
+    resp.end("Hello World\n");
+}).listen(8888);
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
